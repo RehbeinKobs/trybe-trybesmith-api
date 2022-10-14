1349134fb76637e283ed9ba1a5c1543fa23bfc9d
Vinicius Kobs <viniciusrehbeinkobs@gmail.com>
2022-10-14 18:46:07 -0300

    requisito 5 arrumado

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -24,6 +24,11 @@ export default class UserService {
   }
 
   public async login(user: IUser): Promise<number> {
+    const { username, password } = user;
+
+    if (!username) throw throwError(400, '"username" is required');
+    if (!password) throw throwError(400, '"password" is required');
+
     const result = await this.model.login(user);
     if (result) return result;
     throw throwError(401, 'Username or password invalid');
