522b37b8221e1642750e0b7649c45d5d92bc893d
Vinicius Kobs <viniciusrehbeinkobs@gmail.com>
2022-10-14 18:08:10 -0300

    requisito 6

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,6 +1,7 @@
 import connection from '../models/connection';
 import ProductModel from '../models/product.models';
 import { IProduct } from '../interfaces';
+import throwError from '../utils/throwError';
 
 export default class ProductService {
   public model: ProductModel;
@@ -9,12 +10,25 @@ export default class ProductService {
     this.model = new ProductModel(connection);
   }
 
+  private validateField = (field: string, name: string) => {
+    if (!field) throw throwError(400, `"${name}" is required`);
+    if (typeof field !== 'string') throw throwError(422, `"${name}" must be a string`);
+    if (field.length < 3) {
+      throw throwError(422, `"${name}" length must be at least 3 characters long`);
+    }
+  };
+
   public async getAll(): Promise<IProduct[]> {
     const products = await this.model.getAll();
     return products;
   }
 
   public async create(product: IProduct): Promise<IProduct> {
+    const { name, amount } = product;
+
+    this.validateField(name, 'name');
+    this.validateField(amount, 'amount');
+
     const result = await this.model.create(product);
     return result;
   }
